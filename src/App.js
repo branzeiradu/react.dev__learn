f7f45460e8c3e7e5ee0c7e001dd8d118994f658d
Branzei Radu <branzeiradu@gmail.com>
2024-02-09 23:32:08 +0200

    react.dev/learn_tic_tact_toe: lift up square state to board component level

--- src/App.js
+++ src/App.js
@@ -2,33 +2,36 @@ import { useState } from 'react';
 
 const X = "X", O = "O";
 
-export function Square() {
-  const [letter, setLetter] = useState(null);
-
-  const clickHandler = function () {
-    setLetter((letter == null) ? X : null);
-  }
-  
+export function Square({ letter, clickHandler }) {
   return <button className="square" onClick={clickHandler}>{letter}</button>;
 }
 
 export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(squareIndex) {
+    const nextSquares = squares.slice();
+    nextSquares[squareIndex] = X;
+    setSquares(nextSquares);
+  }
+
   return (
     <div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square letter={squares[0]} clickHandler={() => handleClick(0)} />
+        <Square letter={squares[1]} clickHandler={() => handleClick(1)} />
+        <Square letter={squares[2]} clickHandler={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square letter={squares[3]} clickHandler={() => handleClick(3)} />
+        <Square letter={squares[4]} clickHandler={() => handleClick(4)} />
+        <Square letter={squares[5]} clickHandler={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square letter={squares[6]} clickHandler={() => handleClick(6)} />
+        <Square letter={squares[7]} clickHandler={() => handleClick(7)} />
+        <Square letter={squares[8]} clickHandler={() => handleClick(8)} />
       </div>
     </div>
   )
