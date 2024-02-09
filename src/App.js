889c2c8a3fcaf562cab1da0382fa5eb953584fd9
Branzei Radu <branzeiradu@gmail.com>
2024-02-09 22:57:11 +0200

    react.dev/learn_tic_tac_toe: add board component, add state to square component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from 'react';
+
+const X = "X", O = "O";
+
+export function Square() {
+  const [letter, setLetter] = useState(null);
+
+  const clickHandler = function () {
+    setLetter((letter == null) ? X : null);
+  }
+  
+  return <button className="square" onClick={clickHandler}>{letter}</button>;
+}
+
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  )
 }
